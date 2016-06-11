266ff8928045c5dac7609cd9f355981911b0cea1
Tristan Hume <tris.hume@gmail.com>
2016-06-11 17:45:35 -0400

    Use flate2 compression for loading dumps.

--- src/dumps.rs
+++ src/dumps.rs
@@ -1,9 +1,12 @@
 use bincode::SizeLimit;
 use bincode::rustc_serialize::*;
 use std::fs::File;
-use std::io::BufReader;
+use std::io::{BufReader, BufWriter};
 use package_set::PackageSet;
 use std::path::Path;
+use flate2::write::ZlibEncoder;
+use flate2::read::ZlibDecoder;
+use flate2::Compression;
 
 impl PackageSet {
     /// Instantiates a new package set from a binary dump of
@@ -26,7 +29,7 @@ impl PackageSet {
 
     /// Same as `load_defaults_nonewlines` but for parsing line strings with newlines at the end.
     /// These are separate methods because thanks to linker garbage collection, only the serialized
-    /// dumps for the method(s) you call will be included in the binary (each is ~1MB for now).
+    /// dumps for the method(s) you call will be included in the binary (each is ~200kb for now).
     pub fn load_defaults_newlines() -> PackageSet {
         let mut ps = Self::from_binary(include_bytes!("../assets/default_newlines.packdump"));
         ps.link_syntaxes();
@@ -35,18 +38,25 @@ impl PackageSet {
 
     pub fn dump_binary(&self) -> Vec<u8> {
         assert!(!self.is_linked);
-        encode(self, SizeLimit::Infinite).unwrap()
+        let mut v = Vec::new();
+        {
+            let mut encoder = ZlibEncoder::new(&mut v, Compression::Best);
+            encode_into(self, &mut encoder, SizeLimit::Infinite).unwrap();
+        }
+        v
     }
 
     pub fn dump_to_file<P: AsRef<Path>>(&self, path: P) -> EncodingResult<()> {
-        let mut f = try!(File::create(path).map_err(EncodingError::IoError));
-        encode_into(self, &mut f, SizeLimit::Infinite)
+        let f = BufWriter::new(try!(File::create(path).map_err(EncodingError::IoError)));
+        let mut encoder = ZlibEncoder::new(f, Compression::Best);
+        encode_into(self, &mut encoder, SizeLimit::Infinite)
     }
 
     /// Returns a fully loaded and linked package set from
     /// a binary dump. Panics if the dump is invalid.
     pub fn from_binary(v: &[u8]) -> PackageSet {
-        let mut ps: PackageSet = decode(v).unwrap();
+        let mut decoder = ZlibDecoder::new(v);
+        let mut ps: PackageSet = decode_from(&mut decoder, SizeLimit::Infinite).unwrap();
         ps.link_syntaxes();
         ps
     }
@@ -55,8 +65,8 @@ impl PackageSet {
     /// a binary dump file.
     pub fn from_dump_file<P: AsRef<Path>>(path: P) -> DecodingResult<PackageSet> {
         let f = try!(File::open(path).map_err(DecodingError::IoError));
-        let mut reader = BufReader::new(f);
-        decode_from(&mut reader, SizeLimit::Infinite)
+        let mut decoder = ZlibDecoder::new(BufReader::new(f));
+        decode_from(&mut decoder, SizeLimit::Infinite)
     }
 }
 
