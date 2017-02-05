cb9b00cb933549093973d81e2fb0f7a2ffd8e516
Tristan Hume <tris.hume@gmail.com>
2017-02-04 19:53:33 -0500

    Issue 33 test case

---
+++ testdata/issue33.rs
@@ -0,0 +1,3 @@
+// issue #33: it prints this line and then hangs on the next one, hang time increases super-linearly with line length
+impl ApplicationPreferenceseeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee {
+}
