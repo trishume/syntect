ef3b99b06b72e89b7a0036969897751034422f5a
Keith Hall <kingkeith+github@gmail.com>
2017-03-16 10:22:44 +0200

    remove duplication from tests

--- src/parsing/parser.rs
+++ src/parsing/parser.rs
@@ -484,7 +484,25 @@ mod tests {
         // assert!(false);
     }
 
-    fn expect_scope_stack(line: &str, state: &mut ParseState, stack: &mut ScopeStack, expect: &[&str]) {
+    fn expect_scope_stacks(line: &str, expect: &[&str]) {
+        // check that each expected scope stack appears at least once while parsing the given test line
+        
+        //let syntax = SyntaxSet::load_syntax_file("testdata/parser_tests.sublime-syntax", true).unwrap();
+        use std::fs::File;
+        use std::io::Read;
+        let mut f = File::open("testdata/parser_tests.sublime-syntax").unwrap();
+        let mut s = String::new();
+        f.read_to_string(&mut s).unwrap();
+
+        let syntax = SyntaxDefinition::load_from_str(&s, true).unwrap();
+        
+        let mut state = ParseState::new(&syntax);
+
+        let mut ss = SyntaxSet::new();
+        ss.add_syntax(syntax);
+        ss.link_syntaxes();
+
+        let mut stack = ScopeStack::new();
         let ops = state.parse_line(line);
         debug_print_ops(line, &ops);
 
@@ -506,233 +524,55 @@ mod tests {
 
     #[test]
     fn can_parse_non_nested_clear_scopes() {
-        let syntax: SyntaxDefinition =
-            SyntaxDefinition::load_from_str(r#"
-                name: Simple Clear Scopes Test
-                scope: source.example
-                contexts:
-                  main:
-                    - match: \'
-                      scope: punctuation.definition.string.begin.example
-                      push: single_quoted_string
-                  single_quoted_string:
-                    - meta_scope: string.quoted.single.example
-                    - match: 'test'
-                      scope: example.pushes-clear-scopes.example
-                      push:
-                        - clear_scopes: 1
-                        - meta_scope: example.meta-scope.after-clear-scopes.example
-                        - match: 'test'
-                          scope: example.pops-clear-scopes.example
-                          pop: true
-                    - match: '\\.'
-                      scope: constant.character.escape.example
-                    - match: \'
-                      scope: punctuation.definition.string.end.example
-                      pop: true
-
-                "#, true)
-                .unwrap();
-
-        let line = "'hello test world test \\n '\n";
-        let mut state = ParseState::new(&syntax);
-
-        let mut ss = SyntaxSet::new();
-        ss.add_syntax(syntax);
-        ss.link_syntaxes();
-
-        let mut stack = ScopeStack::new();
+        let line = "'hello #simple_cleared_scopes_test world test \\n '\n";
         let expect = [
-            "<source.example>, <example.meta-scope.after-clear-scopes.example>, <example.pushes-clear-scopes.example>",
-            "<source.example>, <example.meta-scope.after-clear-scopes.example>, <example.pops-clear-scopes.example>",
-            "<source.example>, <string.quoted.single.example>, <constant.character.escape.example>",
+            "<source.test>, <example.meta-scope.after-clear-scopes.example>, <example.pushes-clear-scopes.example>",
+            "<source.test>, <example.meta-scope.after-clear-scopes.example>, <example.pops-clear-scopes.example>",
+            "<source.test>, <string.quoted.single.example>, <constant.character.escape.example>",
         ];
-        expect_scope_stack(&line, &mut state, &mut stack, &expect);
+        expect_scope_stacks(&line, &expect);
     }
 
     #[test]
     fn can_parse_non_nested_too_many_clear_scopes() {
-        let syntax: SyntaxDefinition =
-            SyntaxDefinition::load_from_str(r#"
-                name: Simple Clear Scopes Test
-                scope: source.example
-                contexts:
-                  main:
-                    - match: \'
-                      scope: punctuation.definition.string.begin.example
-                      push: single_quoted_string
-                  single_quoted_string:
-                    - meta_scope: string.quoted.single.example
-                    - match: 'test'
-                      scope: example.pushes-clear-scopes.example
-                      push:
-                        - clear_scopes: 10
-                        - meta_scope: example.meta-scope.after-clear-scopes.example
-                        - match: 'test'
-                          scope: example.pops-clear-scopes.example
-                          pop: true
-                    - match: '\\.'
-                      scope: constant.character.escape.example
-                    - match: \'
-                      scope: punctuation.definition.string.end.example
-                      pop: true
-
-                "#, true)
-                .unwrap();
-
-        let line = "'hello test world test \\n '\n";
-        let mut state = ParseState::new(&syntax);
-
-        let mut ss = SyntaxSet::new();
-        ss.add_syntax(syntax);
-        ss.link_syntaxes();
-
-        let mut stack = ScopeStack::new();
+        let line = "'hello #too_many_cleared_scopes_test world test \\n '\n";
         let expect = [
             "<example.meta-scope.after-clear-scopes.example>, <example.pushes-clear-scopes.example>",
             "<example.meta-scope.after-clear-scopes.example>, <example.pops-clear-scopes.example>",
-            "<source.example>, <string.quoted.single.example>, <constant.character.escape.example>",
+            "<source.test>, <string.quoted.single.example>, <constant.character.escape.example>",
         ];
-        expect_scope_stack(&line, &mut state, &mut stack, &expect);
+        expect_scope_stacks(&line, &expect);
     }
 
     #[test]
     fn can_parse_nested_clear_scopes() {
-        let syntax: SyntaxDefinition =
-            SyntaxDefinition::load_from_str(r#"
-                name: Simple Clear Scopes Test
-                scope: source.example
-                contexts:
-                  main:
-                    - match: \'
-                      scope: punctuation.definition.string.begin.example
-                      push: single_quoted_string
-                  single_quoted_string:
-                    - meta_scope: string.quoted.single.example
-                    - match: 'test'
-                      scope: example.pushes-clear-scopes.example
-                      push:
-                        - clear_scopes: 1
-                        - meta_scope: example.meta-scope.after-clear-scopes.example
-                        - match: 'foo'
-                          scope: foo
-                          push:
-                            - clear_scopes: 1
-                            - meta_scope: example.meta-scope.cleared-previous-meta-scope.example
-                            - match: 'bar'
-                              scope: bar
-                              pop: true
-                        - match: 'test'
-                          scope: example.pops-clear-scopes.example
-                          pop: true
-                    - match: '\\.'
-                      scope: constant.character.escape.example
-                    - match: \'
-                      scope: punctuation.definition.string.end.example
-                      pop: true
-
-                "#, true)
-                .unwrap();
-
-        let line = "'hello test world foo bar test \\n '\n";
-        let mut state = ParseState::new(&syntax);
-
-        let mut ss = SyntaxSet::new();
-        ss.add_syntax(syntax);
-        ss.link_syntaxes();
-
-        let mut stack = ScopeStack::new();
+        let line = "'hello #nested_clear_scopes_test world foo bar test \\n '\n";
         let expect = [
-            "<source.example>, <example.meta-scope.after-clear-scopes.example>, <example.pushes-clear-scopes.example>",
-            "<source.example>, <example.meta-scope.cleared-previous-meta-scope.example>, <foo>",
-            "<source.example>, <example.meta-scope.after-clear-scopes.example>, <example.pops-clear-scopes.example>",
-            "<source.example>, <string.quoted.single.example>, <constant.character.escape.example>",
+            "<source.test>, <example.meta-scope.after-clear-scopes.example>, <example.pushes-clear-scopes.example>",
+            "<source.test>, <example.meta-scope.cleared-previous-meta-scope.example>, <foo>",
+            "<source.test>, <example.meta-scope.after-clear-scopes.example>, <example.pops-clear-scopes.example>",
+            "<source.test>, <string.quoted.single.example>, <constant.character.escape.example>",
         ];
-        expect_scope_stack(&line, &mut state, &mut stack, &expect);
+        expect_scope_stacks(&line, &expect);
     }
 
     #[test]
     fn can_parse_infinite_loop() {
-        let syntax: SyntaxDefinition =
-            SyntaxDefinition::load_from_str(r#"
-                name: Infinite Push Pop Loop Test
-                scope: source.test
-                contexts:
-                  main:
-                    - match: '(?=abc)'
-                      push: pop_if_not_whitespace
-                    - match: '\d+'
-                      scope: constant.numeric.test
-
-                  pop_if_not_whitespace:
-                    - match: '(?=\S)'
-                      pop: true
-                "#, true)
-                .unwrap();
-
-        let mut state = ParseState::new(&syntax);
-
-        let mut ss = SyntaxSet::new();
-        ss.add_syntax(syntax);
-        ss.link_syntaxes();
-
-        let mut stack = ScopeStack::new();
-
-        let line = "abc 123\n";
+        let line = "#infinite_loop_test 123\n";
         let expect = [
             "<source.test>, <constant.numeric.test>",
         ];
-        expect_scope_stack(&line, &mut state, &mut stack, &expect);
+        expect_scope_stacks(&line, &expect);
     }
 
     #[test]
     fn can_parse_infinite_seeming_loop() {
-        let syntax: SyntaxDefinition =
-            SyntaxDefinition::load_from_str(r#"
-                name: Non-Infinite Push Pop Loop Test
-                scope: source.test
-                contexts:
-                  main:
-                    - match: 'test'
-                      scope: constant.numeric.test
-                      push: c
-                  a:
-                    - meta_content_scope: test
-                    - match: 'h'
-                      scope: string.unquoted.test
-                    - match: 'ello'
-                      scope: keyword.control.test
-                  b:
-                    - match: ''
-                      pop: true
-                    - match: '(?=.)'
-                      pop: true
-                    - match: '(?=h)'
-                      pop: true
-                    - match: 'h'
-                      scope: entity.name.function.test
-                    - match: 'e'
-                      scope: storage.type.test
-                  c:
-                    - match: ''
-                      push: [a, b]
-                "#, true)
-                .unwrap();
-
-        let mut state = ParseState::new(&syntax);
-
-        let mut ss = SyntaxSet::new();
-        ss.add_syntax(syntax);
-        ss.link_syntaxes();
-
-        let mut stack = ScopeStack::new();
-
-        let line = "testhello\n";
+        let line = "#infinite_seeming_loop_test hello\n";
         let expect = [
-            "<source.test>, <constant.numeric.test>",
+            "<source.test>, <keyword.test>",
             "<source.test>, <test>, <string.unquoted.test>",
             "<source.test>, <test>, <keyword.control.test>",
         ];
-        expect_scope_stack(&line, &mut state, &mut stack, &expect);
+        expect_scope_stacks(&line, &expect);
     }
 }
