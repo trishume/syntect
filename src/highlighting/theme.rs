27169fea686fa84af09f8a943424368657156b07
Colin Rofls <colin@cmyr.net>
2017-06-20 12:48:58 -0400

    Fix issues in theme.rs and update themedump

--- src/highlighting/theme.rs
+++ src/highlighting/theme.rs
@@ -25,15 +25,27 @@ pub struct Theme {
 /// Use this to make your editor UI match the highlighted text.
 #[derive(Clone, Debug, Default, Serialize, Deserialize)]
 pub struct ThemeSettings {
-    /// Foreground color for the view.
+    /// The default color for text.
     pub foreground: Option<Color>,
-    /// Backgound color of the view.
+    /// The default backgound color of the view.
     pub background: Option<Color>,
     /// Color of the caret.
     pub caret: Option<Color>,
     /// Color of the line the caret is in.
     /// Only used when the `higlight_line` setting is set to `true`.
     pub line_highlight: Option<Color>,
+
+    /// The color to use for the squiggly underline drawn under misspelled words.
+    pub misspelling: Option<Color>,
+    /// The color of the border drawn around the viewport area of the minimap.
+    /// Only used when the `draw_minimap_border` setting is enabled.
+    pub minimap_border: Option<Color>,
+    /// A color made available for use by the theme.
+    pub accent: Option<Color>,
+    /// CSS passed to popups.
+    pub popup_css: Option<Color>,
+    /// CSS passed to phantoms.
+    pub phantom_css: Option<Color>,
 
     /// Color of bracketed sections of text when the caret is in a bracketed section.
     /// Only applied when the `match_brackets` setting is set to `true`.
@@ -58,9 +70,11 @@ pub struct ThemeSettings {
     /// Only applied when the match_tags setting is set to `true`.
     pub tags_options: Option<UnderlineOption>,
 
+    /// The border color for "other" matches.
+    pub highlight: Option<Color>,
     /// Background color of regions matching the current search.
     pub find_highlight: Option<Color>,
-    /// Background color of regions matching the current search.
+    /// Text color of regions matching the current search.
     pub find_highlight_foreground: Option<Color>,
 
     /// Background color of the gutter.
@@ -68,14 +82,25 @@ pub struct ThemeSettings {
     /// Foreground color of the gutter.
     pub gutter_foreground: Option<Color>,
 
-    /// Color of the selection regions.
+    /// The background color of selected text.
     pub selection: Option<Color>,
-    /// Background color of the selection regions.
+    /// A color that will override the scope-based text color of the selection.
+    pub selection_foreground: Option<Color>,
+
+    /// Deprecated!
+    ///
+    /// This property is not part of the recognized tmTheme format. It may be
+    /// removed in a future release.
     pub selection_background: Option<Color>,
+
     /// Color of the selection regions border.
     pub selection_border: Option<Color>,
-    /// Color of inactive selections (inactive view).
+    /// The background color of a selection in a view that is not currently focused.
     pub inactive_selection: Option<Color>,
+    /// A color that will override the scope-based text color of the selection
+    /// in a view that is not currently focused.
+    pub inactive_selection_foreground: Option<Color>,
+
 
     /// Color of the guides displayed to indicate nesting levels.
     pub guide: Option<Color>,
@@ -86,12 +111,16 @@ pub struct ThemeSettings {
     /// Only used if the `indent_guide_options` setting is set to `draw_active`.
     pub stack_guide: Option<Color>,
 
-    /// Background color for regions added via `sublime.add_regions()`
-    /// with the `sublime.DRAW_OUTLINED` flag added.
-    pub highlight: Option<Color>,
     /// Foreground color for regions added via `sublime.add_regions()`
     /// with the `sublime.DRAW_OUTLINED` flag added.
+    /// 
+    /// Deprecated!
+    /// This setting does not exist in any available documentation.
+    /// Use is discouraged, and it may be removed in a future release.
     pub highlight_foreground: Option<Color>,
+
+    /// The color of the shadow used when a text area can be horizontally scrolled.
+    pub shadow: Option<Color>,
 }
 
 /// A component of a theme meant to highlight a specific thing (e.g string literals)
@@ -324,6 +353,11 @@ impl ParseSettings for ThemeSettings {
                 "background" => settings.background = Color::parse_settings(value).ok(),
                 "caret" => settings.caret = Color::parse_settings(value).ok(),
                 "lineHighlight" => settings.line_highlight = Color::parse_settings(value).ok(),
+                "misspelling" => settings.misspelling = Color::parse_settings(value).ok(),
+                "minimapBorder" => settings.minimap_border = Color::parse_settings(value).ok(),
+                "accent" => settings.accent = Color::parse_settings(value).ok(),
+                "popupCss" => settings.popup_css = Color::parse_settings(value).ok(),
+                "phantomCss" => settings.phantom_css = Color::parse_settings(value).ok(),
                 "bracketContentsForeground" => {
                     settings.bracket_contents_foreground = Color::parse_settings(value).ok()
                 }
@@ -343,6 +377,7 @@ impl ParseSettings for ThemeSettings {
                 "tagsOptions" => {
                     settings.tags_options = UnderlineOption::parse_settings(value).ok()
                 }
+                "highlight" => settings.highlight = Color::parse_settings(value).ok(),
                 "findHighlight" => settings.find_highlight = Color::parse_settings(value).ok(),
                 "findHighlightForeground" => {
                     settings.find_highlight_foreground = Color::parse_settings(value).ok()
@@ -352,20 +387,21 @@ impl ParseSettings for ThemeSettings {
                     settings.gutter_foreground = Color::parse_settings(value).ok()
                 }
                 "selection" => settings.selection = Color::parse_settings(value).ok(),
-                "selectionBackground" => {
-                    settings.selection_background = Color::parse_settings(value).ok()
+                "selectionForeground" => {
+                    settings.selection_foreground = Color::parse_settings(value).ok()
                 }
                 "selectionBorder" => settings.selection_border = Color::parse_settings(value).ok(),
                 "inactiveSelection" => {
                     settings.inactive_selection = Color::parse_settings(value).ok()
                 }
+                "inactiveSelectionForeground" => {
+                    settings.inactive_selection_foreground = Color::parse_settings(value).ok()
+                }
                 "guide" => settings.guide = Color::parse_settings(value).ok(),
                 "activeGuide" => settings.active_guide = Color::parse_settings(value).ok(),
                 "stackGuide" => settings.stack_guide = Color::parse_settings(value).ok(),
-                "highlight" => settings.highlight = Color::parse_settings(value).ok(),
-                "highlightForeground" => {
-                    settings.highlight_foreground = Color::parse_settings(value).ok()
-                }
+                "shadow" => settings.shadow = Color::parse_settings(value).ok(),
+                "shadowWidth"| // ignored
                 "invisibles" | // ignored
                 _ => (),
             }
