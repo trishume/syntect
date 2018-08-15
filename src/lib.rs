eb4e5b7796d6a189f575374068d08175f8f2b639
Vincent Prouillet <prouillet.vincent@gmail.com>
2018-08-15 11:46:08 +0200

    Add filename to ParseSyntax error

--- src/lib.rs
+++ src/lib.rs
@@ -74,7 +74,7 @@ pub enum LoadingError {
     Io(IoError),
     /// a syntax file was invalid in some way
     #[cfg(feature = "yaml-load")]
-    ParseSyntax(ParseSyntaxError),
+    ParseSyntax(ParseSyntaxError, Option<String>),
     /// a theme file was invalid in some way
     ParseTheme(ParseThemeError),
     /// a theme's Plist syntax was invalid in some way
@@ -105,7 +105,7 @@ impl From<ParseThemeError> for LoadingError {
 #[cfg(all(feature = "yaml-load", feature = "parsing"))]
 impl From<ParseSyntaxError> for LoadingError {
     fn from(error: ParseSyntaxError) -> LoadingError {
-        LoadingError::ParseSyntax(error)
+        LoadingError::ParseSyntax(error, None)
     }
 }
 
@@ -117,7 +117,13 @@ impl fmt::Display for LoadingError {
             WalkDir(ref error) => error.fmt(f),
             Io(ref error) => error.fmt(f),
             #[cfg(feature = "yaml-load")]
-            ParseSyntax(ref error) => error.fmt(f),
+            ParseSyntax(ref error, ref filename) => {
+                if let Some(ref file) = filename {
+                    write!(f, "{}: {}", file, error.description())
+                } else {
+                    error.fmt(f)
+                }
+            },
             _ => write!(f, "{}", self.description()),
         }
     }
@@ -131,7 +137,7 @@ impl Error for LoadingError {
             WalkDir(ref error) => error.description(),
             Io(ref error) => error.description(),
             #[cfg(feature = "yaml-load")]
-            ParseSyntax(ref error) => error.description(),
+            ParseSyntax(ref error, ..) => error.description(),
             ParseTheme(_) => "Invalid syntax theme",
             ReadSettings(_) => "Invalid syntax theme settings",
             BadPath => "Invalid path",
