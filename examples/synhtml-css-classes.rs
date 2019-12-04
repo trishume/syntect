3754797a8109402b491dba7fff80fe0ade4365dc
Uwe Arzt <mail@uwe-arzt.de>
2019-12-04 15:53:04 +0100

    implement theme -> css, including example

--- examples/synhtml-css-classes.rs
+++ examples/synhtml-css-classes.rs
@@ -1,39 +1,119 @@
-//! Prints highlighted HTML with CSS classes for a Rust and a C++ file to stdout.
+//! Generates highlighted HTML with CSS classes for a Rust and a C++ source.
 //! Run with ```cargo run --example synhtml-css-classes```
+//!
+//! will generate 4 files as usage example
+//! * synhtml-css-classes.html
+//! * synhtml-css-classes.css
+//! * theme-dark.css
+//! * theme-light.css
+//!
+//! You can open the html with a web browser and change between light and dark
+//! mode.
+use syntect::highlighting::ThemeSet;
+use syntect::html::css_for_theme;
 use syntect::html::ClassedHTMLGenerator;
 use syntect::parsing::SyntaxSet;
 
-fn main() {
+use std::fs::File;
+use std::io::{BufWriter, Write};
+use std::path::Path;
+
+fn main() -> Result<(), std::io::Error> {
+    // ---------------------------------------------------------------------------------------------
+    // generate html
     let ss = SyntaxSet::load_defaults_newlines();
+
+    let html_file =  File::create(Path::new("synhtml-css-classes.html"))?;
+    let mut html = BufWriter::new(&html_file);
+
+    // write html header
+    writeln!(html, "<!DOCTYPE html>")?;
+    writeln!(html, "<html>")?;
+    writeln!(html, "  <head>")?;
+    writeln!(html, "    <title>synhtml-css-classes.rs</title>")?;
+    writeln!(html, "    <link rel=\"stylesheet\" href=\"synhtml-css-classes.css\">")?;
+    writeln!(html, "  </head>")?;
+    writeln!(html, "  <body>")?;
 
     // Rust
     let code_rs = "// Rust source
-    fn main() {
-        println!(\"Hello World!\");
-    }";
+fn main() {
+    println!(\"Hello World!\");
+}";
 
     let sr_rs = ss.find_syntax_by_extension("rs").unwrap();
-    let mut html_generator = ClassedHTMLGenerator::new(&sr_rs, &ss);
+    let mut rs_html_generator = ClassedHTMLGenerator::new(&sr_rs, &ss);
     for line in code_rs.lines() {
-        html_generator.parse_html_for_line(&line);
+        rs_html_generator.parse_html_for_line(&line);
     }
-    let html = html_generator.finalize();
-    println!("{}", html);
+    let html_rs = rs_html_generator.finalize();
 
-    println!("");
+    writeln!(html, "<pre class=\"code\">")?;
+    writeln!(html, "{}", html_rs)?;
+    writeln!(html, "</pre>")?;
 
     // C++
-    let code_cpp = "// C++ source
-    #include <iostream>
-    int main() {
-        std::cout << \"Hello World!\" << std::endl;
-    }";
+    let code_cpp = "/* C++ source */
+#include <iostream>
+int main() {
+    std::cout << \"Hello World!\" << std::endl;
+}";
 
     let sr_cpp = ss.find_syntax_by_extension("cpp").unwrap();
-    let mut html_generator = ClassedHTMLGenerator::new(&sr_cpp, &ss);
+    let mut cpp_html_generator = ClassedHTMLGenerator::new(&sr_cpp, &ss);
     for line in code_cpp.lines() {
-        html_generator.parse_html_for_line(&line);
+        cpp_html_generator.parse_html_for_line(&line);
     }
-    let html = html_generator.finalize();
-    println!("{}", html);
+    let html_cpp = cpp_html_generator.finalize();
+
+    writeln!(html, "<pre class=\"code\">")?;
+    writeln!(html, "{}", html_cpp)?;
+    writeln!(html, "</pre>")?;
+
+    // write html end
+    writeln!(html, "  </body>")?;
+    writeln!(html, "</html>")?;
+
+    // ---------------------------------------------------------------------------------------------
+    // generate css
+    let css = "@import url(\"theme-light.css\") (prefers-color-scheme: light);
+    @import url(\"theme-dark.css\") (prefers-color-scheme: dark);
+
+    @media (prefers-color-scheme: dark) {
+      body {
+        background-color: gray;
+      }
+    }
+    @media (prefers-color-scheme: light) {
+      body {
+        background-color: lightgray;
+      }
+    }";
+
+    let css_file = File::create(Path::new("synhtml-css-classes.css"))?;
+    let mut css_writer = BufWriter::new(&css_file);
+
+    writeln!(css_writer, "{}", css)?;
+
+    // ---------------------------------------------------------------------------------------------
+    // generate css files for themes
+    let ts = ThemeSet::load_defaults();
+
+    // create dark color scheme css
+    let dark_theme = &ts.themes["Solarized (dark)"];
+    let css_dark_file = File::create(Path::new("theme-dark.css"))?;
+    let mut css_dark_writer = BufWriter::new(&css_dark_file);
+
+    let css_dark = css_for_theme(dark_theme);
+    writeln!(css_dark_writer, "{}", css_dark)?;
+
+    // create light color scheme css
+    let light_theme = &ts.themes["Solarized (light)"];
+    let css_light_file = File::create(Path::new("theme-light.css"))?;
+    let mut css_light_writer = BufWriter::new(&css_light_file);
+
+    let css_light = css_for_theme(light_theme);
+    writeln!(css_light_writer, "{}", css_light)?;
+
+    Ok(())
 }
